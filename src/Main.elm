2bb0d547cb1bf222def80af3ffb566dc41e574a9
Bruno Girin <bruno@imby.bio>
2016-09-02 17:29:07 +0100

    IM-35 Added skeleton for IndexedDB research

---
+++ src/Main.elm
@@ -0,0 +1,32 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.App as App
+
+main =
+  App.beginnerProgram { model = model, view = view, update = update }
+
+-- MODEL
+
+type alias Model = Int
+
+model : Model
+model =
+  0
+
+-- UPDATE
+
+type Msg =
+  NoOp
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    NoOp -> model
+
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+  div [] [ text "Hello!" ]
