98ec8e6fce098b8d63a7b0dad4dc44680aa1813a
Bruno Girin <bruno@imby.bio>
2016-09-09 14:48:41 +0100

    IM-35 Cleaned up JS code

--- src/Native/IndexedDB.js
+++ src/Native/IndexedDB.js
@@ -9,19 +9,15 @@ function open(dbname, dbvsn, upgradeneededcallback)
 
         var req = indexedDB.open(dbname, dbvsn);
         req.addEventListener('error', function(evt) {
-            return callback(_elm_lang$core$Native_Scheduler.fail(
-                { ctor: 'RawErrorEvent', _0: evt.type }
-                ));
+            return callback(_elm_lang$core$Native_Scheduler.fail(toErrorEvent(evt)));
         });
         req.addEventListener('blocked', function(evt) {
-            return callback(_elm_lang$core$Native_Scheduler.fail(
-                { ctor: 'RawBlockedEvent', _0: evt }
-                ));
+            return callback(_elm_lang$core$Native_Scheduler.fail(toBlockedEvent(evt)));
         });
         req.addEventListener('upgradeneeded', function(evt) {
             upgradeneededcallback(toVersionchangeEvent(evt))
             // TODO: handle the result of that function
-            // one option could be abort if false
+            // we should probably receive a command to pass to the scheduler
         });
         req.addEventListener('success', function() {
             return callback(_elm_lang$core$Native_Scheduler.succeed(toDatabase(req.result)));
@@ -35,10 +31,14 @@ function open(dbname, dbvsn, upgradeneededcallback)
 function databaseCreateObjectStore(db, osname, osopts)
 {
     var josopts = {
-        autoIncrement: osopts.auto_increment
+        autoIncrement: osopts.auto_increment,
     };
-    if (osopts.key_path.ctor == 'Just') {
-        josopts.keyPath = osopts.key_path._0;
+    // it looks like the object store creation doesn't always work properly
+    // when the keyPath option is provided with a null value, as opposed to
+    // not providng it at all so we check before adding it to the structure
+    var keyPath = fromMaybe(osopts.key_path);
+    if (keyPath != null) {
+        josopts.keyPath = keyPath;
     }
     return toObjectStore(db.createObjectStore(osname, josopts));
 }
@@ -46,60 +46,32 @@ function databaseCreateObjectStore(db, osname, osopts)
 function databaseTransaction(db, snames, mode)
 {
     var jsnames = _elm_lang$core$Native_List.toArray(snames);
-    var tmode = 'readonly';
-    if (mode.ctor == 'ReadWrite') {
-        tmode = 'readwrite';
-    }
+    var tmode = fromTransactionMode(mode);
     try {
-        return {
-            ctor: 'Ok',
-            _0: toTransaction(db.transaction(jsnames, tmode))
-        };
+        return toOkResult(toTransaction(db.transaction(jsnames, tmode)));
     }
     catch(err) {
-        return {
-            ctor: 'Err',
-            _0: {
-                ctor: 'RawDomException',
-                _0: err.code,
-                _1: err.name
-            }
-        };
+        return toErrResult(toDomException(err));
     }
 }
 
 function transactionObjectStore(t, osname)
 {
     try {
-        return {
-            ctor: 'Ok',
-            _0: toObjectStore(t.objectStore(osname))
-        };
+        return toOkResult(toObjectStore(t.objectStore(osname)));
     }
     catch(err) {
-        return {
-            ctor: 'Err',
-            _0: {
-                ctor: 'RawDomException',
-                _0: err.code,
-                _1: err.name
-            }
-        };
+        return toErrResult(toDomException(err));
     }
 }
 
 function objectStoreAdd(os, item, key)
 {
     return _elm_lang$core$Native_Scheduler.nativeBinding(function(callback) {
-        var jkey = null;
-        if (key.ctor == 'Just') {
-            jkey = key._0;
-        }
+        var jkey = fromMaybe(key);
         var req = os.add(item, jkey)
         req.addEventListener('error', function(evt) {
-            return callback(_elm_lang$core$Native_Scheduler.fail(
-                { ctor: 'RawErrorEvent', _0: evt.type }
-                ));
+            return callback(_elm_lang$core$Native_Scheduler.fail(toErrorEvent(evt)));
         });
         req.addEventListener('success', function() {
             return callback(_elm_lang$core$Native_Scheduler.succeed(req.result));
@@ -113,15 +85,10 @@ function objectStoreAdd(os, item, key)
 function objectStorePut(os, item, key)
 {
     return _elm_lang$core$Native_Scheduler.nativeBinding(function(callback) {
-        var jkey = null;
-        if (key.ctor == 'Just') {
-            jkey = key._0;
-        }
+        var jkey = fromMaybe(key);
         var req = os.put(item, jkey)
         req.addEventListener('error', function(evt) {
-            return callback(_elm_lang$core$Native_Scheduler.fail(
-                { ctor: 'RawErrorEvent', _0: evt.type }
-                ));
+            return callback(_elm_lang$core$Native_Scheduler.fail(toErrorEvent(evt)));
         });
         req.addEventListener('success', function() {
             return callback(_elm_lang$core$Native_Scheduler.succeed(req.result));
@@ -137,9 +104,7 @@ function objectStoreDelete(os, key)
     return _elm_lang$core$Native_Scheduler.nativeBinding(function(callback) {
         var req = os.delete(key)
         req.addEventListener('error', function(evt) {
-            return callback(_elm_lang$core$Native_Scheduler.fail(
-                { ctor: 'RawErrorEvent', _0: evt.type }
-                ));
+            return callback(_elm_lang$core$Native_Scheduler.fail(toErrorEvent(evt)));
         });
         req.addEventListener('success', function() {
             return callback(_elm_lang$core$Native_Scheduler.succeed(key));
@@ -155,22 +120,18 @@ function objectStoreGet(os, key)
     return _elm_lang$core$Native_Scheduler.nativeBinding(function(callback) {
         var req = os.get(key)
         req.addEventListener('error', function(evt) {
-            return callback(_elm_lang$core$Native_Scheduler.fail(
-                { ctor: 'RawErrorEvent', _0: evt.type }
-                ));
+            return callback(_elm_lang$core$Native_Scheduler.fail(toErrorEvent(evt)));
         });
         req.addEventListener('success', function() {
-            var eresult = { ctor: 'Nothing' }
-            if (req.result != null) {
-                eresult = { ctor: 'Just', _0: req.result }
-            }
-            return callback(_elm_lang$core$Native_Scheduler.succeed(eresult));
+            return callback(_elm_lang$core$Native_Scheduler.succeed(toMaybe(req.result)));
         });
 
         return function() {
         };
     });
 }
+
+// Structure returned values into Elm friendly objects
 
 function toVersionchangeEvent(evt) {
     return {
@@ -198,16 +159,70 @@ function toObjectStore(os) {
 }
 
 function toTransaction(t) {
-    // readwriteflush is Firefox specific, mapping to ReadWrite
-    var tctor = 'ReadOnly';
-    if (t.mode == 'readwrite' || t.mode == 'readwriteflush') {
-        tctor = 'ReadWrite';
-    }
     return {
-        mode: { ctor: tctor },
+        mode: toTransactionMode(t.mode),
         object_store_names: _elm_lang$core$Native_List.fromArray(t.objectStoreNames),
         handle: t
     }
+}
+
+// Transform simple structure to and from Elm
+
+function fromMaybe(m) {
+    if (m.ctor == 'Just') {
+        return m._0;
+    } else {
+        return null;
+    }
+}
+
+function toMaybe(v) {
+    if (v == null) {
+        return { ctor: 'Nothing' };
+    } else {
+        return { ctor: 'Just', _0: v };
+    }
+}
+
+function fromTransactionMode(m) {
+    if (m.ctor == 'ReadWrite') {
+        return 'readwrite';
+    } else {
+        return 'readonly';
+    }
+}
+
+function toTransactionMode(v) {
+    // readwriteflush is Firefox specific, mapping to ReadWrite
+    if (v == 'readwrite' || v == 'readwriteflush') {
+        return { ctor: 'ReadWrite' };
+    } else {
+        return { ctor: 'ReadOnly' };
+    }
+}
+
+function toDomException(err) {
+    return {
+        ctor: 'RawDomException',
+        _0: err.code,
+        _1: err.name
+    };
+}
+
+function toErrorEvent(evt) {
+    return { ctor: 'RawErrorEvent', _0: evt.type };
+}
+
+function toBlockedEvent(evt) {
+    return { ctor: 'RawBlockedEvent', _0: evt };
+}
+
+function toOkResult(v) {
+    return { ctor: 'Ok', _0: v };
+}
+
+function toErrResult(err) {
+    return { ctor: 'Err', _0: err };
 }
 
 return {
